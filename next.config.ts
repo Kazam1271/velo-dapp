dd5fc083b08328d6d68dfe94c3e7e9fbb4df0e41
Kazam1271 <kazam1271@users.noreply.github.com>
2026-04-24 14:19:07 +0100

    Revert next.config.ts and rely on dynamic imports

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        crypto: false,
-        fs: false,
-        path: false,
-        os: false,
-        net: false,
-        tls: false,
-      };
-    } else {
-      config.externals.push('crypto');
-    }
-    return config;
-  },
+  /* config options here */
 };
 
 export default nextConfig;
